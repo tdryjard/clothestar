d662c10f74be094e3d99d167744e3b5ee93551d2
Thomas.dryjard <newwbuiss@gmail.com>
2020-11-11 18:50:23 +0100

    fix

--- src/components/buy/Buy.tsx
+++ src/components/buy/Buy.tsx
@@ -7,7 +7,7 @@ import {Navbar} from '../navbar/Navbar'
 
 export const Buy = ({ panier, price }: any) => {
 
-    const stripePromise = loadStripe("pk_test_AGb35S7bWUgRgRUh3tsxgfrL00MDuBTKPS");
+    const stripePromise = loadStripe("pk_live_u4e03SLJFJMC8k4Bv7g1T3Py00rrpMeJLo");
 
     return (
         <div className="containerElementPayment">
